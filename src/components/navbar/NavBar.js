59a9f86da9d37147585931e7de35e74878ebd3f7
MilanP5G <milan_parmar@hotmail.co.uk>
2021-08-02 14:52:18 +0100

    imported company logo into navbar and styled

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import '../../styling/navbar.css'
+import { NavLink } from 'react-router-dom';
+import navLogo from '../../assets/seedlegals-logo.png';
+import '../../styling/navbar.css';
 
 const NavBar = () => {
 
   return (
     <div className='nav-bar'>
-
+      <div className='nav-logo'>
+        <NavLink className='nav-seedlegal-logo' to='/'>
+          <img id='nav-logo' alt='Seed Legals' src={navLogo} />
+        </NavLink>
+      </div>
     </div>
   )
 
