96a3a4ec80efa375e25b6442680b38dfc41fd129
MilanP5G <milan_parmar@hotmail.co.uk>
2021-08-02 21:48:17 +0100

    added feature images and styled

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import shapes from '../../assets/intro-shapes.png';
+import seis from '../../assets/apply-for-seis.png';
+import raise from '../../assets/raise-before.png';
+import funding from '../../assets/do-a-funding-round.png';
+import policy from '../../assets/company-policies.png';
+import share from '../../assets/share-option-scheme.png';
+import team from '../../assets/team-agreement.png';
+import table from '../../assets/cap-table.png';
+import compliance from '../../assets/seis-compliance.png';
 import '../../styling/main.css';
 
 const Main = () => {
@@ -19,20 +27,28 @@ const Main = () => {
       </div>
       <div className='origin-features'>
         <div className='feature-assurance'>
+          <img alt="seis" src={seis} />
         </div>
         <div className='feature-raise'>
+          <img alt="raise" src={raise} />
         </div>
         <div className='feature-funding'>
+          <img alt="funding" src={funding} />
         </div>
         <div className='feature-emi'>
+          <img alt="policy" src={policy} />
         </div>
         <div className='feature-topup'>
+          <img alt="share" src={share} />
         </div>
         <div className='feature-cap'>
+          <img alt="table" src={table} />
         </div>
         <div className='feature-tax'>
+          <img alt="compliance" src={compliance} />
         </div>
         <div className='feature-contracts'>
+          <img alt="team" src={team} />
         </div>
       </div>
     </div>
