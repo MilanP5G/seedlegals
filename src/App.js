05274fb2e7da55b6aaf568a8dd2d618fdd61349d
MilanP5G <milan_parmar@hotmail.co.uk>
2021-08-02 14:44:51 +0100

    installed react-router-dom and added navbar component to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Main from './components/main/Main'
+import { BrowserRouter as Router } from 'react-router-dom';
+import NavBar from './components/navbar/NavBar';
+import Main from './components/main/Main';
 import './App.css';
 
 const App = () => {
 
   return (
-    <Main />
+    <Router>
+      <div>
+        <NavBar />
+        <Main />
+      </div>
+    </ Router>
   )
 
 }
